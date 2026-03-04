f8114d65926e930f1dfe331e5ac4374e2b372a1a
SpartanA259 <cartergchao@yahoo.com>
2026-03-04 16:50:14 -0600

    Add personal introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Carter Chao" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hey everyone, my name is Carter Chao, and I am a junior at Stratford High School in Houston, Texas. I have zero experience in coding (except for scratch but that really doesn't count), so hopefully I don't get wrecked by the python course. I am interested in engineering and aerospace, and I've had a couple engineering projects on the side like woodworking projects, modifying nerf guns, and building PCs. Outside of that, I like to play tennis, volleyball, and basketball, and I like to ride motorcycles. I am excited to learn with you all.")
 
 if __name__ == "__main__":
     main()
